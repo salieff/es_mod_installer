15520107d4892b068656510b86eb08048feecb0f
Alexander S. Salieff <salieff@mail.ru>
2016-01-29 06:42:06 +0300

    Base dumb implementations

---
+++ persons_web/db_create.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from config import SQLALCHEMY_DATABASE_URI
+from app import db
+import os.path
+
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+db.create_all()
