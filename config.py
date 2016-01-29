694b6b2d8d59045da11e15582669265aa94035fb
Alexander S. Salieff <salieff@mail.ru>
2016-01-30 00:01:51 +0300

    Implemented working DB filling

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'modpersons.db')
-JSON_INDEX_PATH = "/static/project1.json"
+JSON_INDEX_PATH = "http://191.ru/es/project1.json"
