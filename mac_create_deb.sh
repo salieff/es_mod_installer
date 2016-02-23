f7255564f82f2d3ab0e310865def72fc0fad389e
Alexander S. Salieff <salieff@mail.ru>
2016-02-24 02:30:56 +0300

    Deb packaging infrastucture improvements

--- mac_create_deb.sh
+++ mac_create_deb.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-fakeroot dpkg-deb -Zgzip --build esmanager-deb esmanager_0.7-2_iphoneos-arm.deb
+set -xe
+
+rm -rf /tmp/deb-package
+cp -r deb-package /tmp
+unzip "$1" -d /tmp/deb-package
+rm -rf /tmp/deb-package/Applications/ESManager.app
+mv -f /tmp/deb-package/Payload/ESManager.app /tmp/deb-package/Applications/
+rm -rf /tmp/deb-package/Payload
+find /tmp/deb-package/Applications -type f -exec md5sum '{}' ';' | sed -e 's;/tmp/deb-package;;' > /tmp/deb-package/DEBIAN/md5sums
+
+VERS=`cat /tmp/deb-package/DEBIAN/control | grep 'Version:' | sed -e 's/Version: //'`
+
+fakeroot dpkg-deb -Zgzip --build /tmp/deb-package /tmp/esmanager_"$VERS"_iphoneos-arm.deb
