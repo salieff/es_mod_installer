a26d05b92c9ba8857fc6578954df065867f2c393
Alexander S. Salieff <salieff@mail.ru>
2016-08-23 16:47:13 +0300

    Preparing for iOS build with root privilegies

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QQuickWindow>
 #include <QQmlContext>
 #include <QtGlobal>
+#include <QMessageBox>
 
 #if !defined(ANDROID) && !defined(Q_OS_IOS)
 #include <QtWebEngine>
@@ -15,7 +16,31 @@
 
 int main(int argc, char *argv[])
 {
+#if defined(Q_OS_IOS)
+    int iosLastErr = 0;
+    QString iosErrStr;
+
+    if (setuid(0) != 0 || setgid(0) != 0)
+    {
+        iosLastErr = errno;
+        char errBuff[4096] = {0};
+
+        if (strerror_r(iosLastErr, errBuff, 4095) != 0)
+            iosErrStr = "Unknown error";
+        else
+            iosErrStr = QString::fromLocal8Bit(errBuff);
+    }
+#endif
+
     QApplication app(argc, argv);
+
+#if defined(Q_OS_IOS)
+    if (iosLastErr != 0)
+    {
+        QMessageBox::critical(NULL, QObject::tr("Error"), QString(QObject::tr("Can't reach root privilegies : %1 (%2)")).arg(iosErrStr).arg(iosLastErr));
+        exit(EXIT_FAILURE);
+    }
+#endif
 
     AsyncDownloader::createNetworkManager(&app);
     StatisticsManager::getInstance(&app);
