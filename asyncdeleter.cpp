a373c4050b057c067c578dd72af4607df7d4fd27
Alexander S. Salieff <salieff@mail.ru>
2025-05-11 14:18:42 +0200

    Delete с обходлм дерева и прогрессом. Работает медленно :(

--- asyncdeleter.cpp
+++ asyncdeleter.cpp
@@ -2,7 +2,7 @@
 #include <QFile>
 #include <QFileInfo>
 
-#include <set>
+#include <android/log.h>
 
 #include "asyncdeleter.h"
 #include "safadapter.h"
@@ -22,27 +22,94 @@ bool AsyncDeleter::deleteFiles(QStringList flist)
 
 void AsyncDeleter::run()
 {
-    std::set<QString> localPaths;
+    qint64 totalElements = 0;
+    qint64 remainsElements = 0;
+    int progress = 100;
+    FolderTreeElement elementsForDelete(this, &totalElements, &remainsElements, &progress);
 
-    for (const auto &fname: qAsConst(m_localFiles))
+    for (const auto &fpath: qAsConst(m_localFiles))
+        elementsForDelete.addFilePath(fpath);
+
+    elementsForDelete.deleteFolderTree();
+}
+
+
+AsyncDeleter::FolderTreeElement::FolderTreeElement(const AsyncDeleter *deleter,
+                                                   qint64 *totalElements,
+                                                   qint64 *remainsElements,
+                                                   int *progress)
+    : m_deleter(deleter)
+    , m_totalElements(totalElements)
+    , m_remainsElements(remainsElements)
+    , m_progress(progress)
+{
+}
+
+void AsyncDeleter::FolderTreeElement::addFilePath(const QString &path)
+{
+    addFilePathAsList(QFileInfo(path).dir().path().split('/', Qt::SkipEmptyParts), QFileInfo(path).fileName());
+}
+
+void AsyncDeleter::FolderTreeElement::addFilePathAsList(const QStringList &foldersList, const QString &fname)
+{
+    if (foldersList.empty())
     {
-        auto foldersList = QFileInfo(fname).dir().path().split('/', Qt::SkipEmptyParts);
+        m_files.push_back(fname);
+        ++(*m_totalElements);
+        ++(*m_remainsElements);
 
-        if (foldersList.empty())
+        if (fname.endsWith(".rpy", Qt::CaseInsensitive))
         {
-            SafAdapter::getCurrentAdapter().DeleteFile(fname);
-            if (fname.endsWith(".rpy", Qt::CaseInsensitive))
-            {
-                SafAdapter::getCurrentAdapter().DeleteFile(fname + "c");
-                SafAdapter::getCurrentAdapter().DeleteFile(fname + "C");
-            }
+            m_files.push_back(fname + "c");
+            m_files.push_back(fname + "C");
+
+            (*m_totalElements) += 2;
+            (*m_remainsElements) += 2;
         }
-        else
-        {
-            localPaths.insert(foldersList.front());
-        }
+
+        return;
     }
 
-    for (auto const &path : localPaths)
-        SafAdapter::getCurrentAdapter().DeleteFolder(path);
+    auto it = m_folders.find(foldersList.front());
+    if (it == m_folders.end())
+    {
+        bool b;
+        std::tie(it, b) = m_folders.emplace(std::piecewise_construct,
+                                            std::forward_as_tuple(foldersList.front()),
+                                            std::forward_as_tuple(m_deleter, m_totalElements, m_remainsElements, m_progress));
+        ++(*m_totalElements);
+        ++(*m_remainsElements);
+    }
+
+    it->second.addFilePathAsList(foldersList.mid(1), fname);
+}
+
+void AsyncDeleter::FolderTreeElement::deleteFolderTree(const QString &rootFolder)
+{
+    for (auto &subFolder : m_folders)
+    {
+        // subFolder.second.deleteFolderTree(rootFolder + "/" + subFolder.first);
+        SafAdapter::getCurrentAdapter().DeleteFolder(rootFolder + "/" + subFolder.first);
+        __android_log_write(ANDROID_LOG_DEBUG, "deleteFolderTree.DeleteFolder", (rootFolder + "/" + subFolder.first).toLocal8Bit().constData());
+        decreaseProgress();
+    }
+
+    for (const auto &file : qAsConst(m_files))
+    {
+        SafAdapter::getCurrentAdapter().DeleteFile(rootFolder + "/" + file);
+        __android_log_write(ANDROID_LOG_DEBUG, "deleteFolderTree.DeleteFile", (rootFolder + "/" + file).toLocal8Bit().constData());
+        decreaseProgress();
+    }
+}
+
+void AsyncDeleter::FolderTreeElement::decreaseProgress()
+{
+    --(*m_remainsElements);
+    int currentProgress = (*m_totalElements) > 0 ? (*m_remainsElements) * 100 / (*m_totalElements) : 0;
+
+    if (currentProgress == (*m_progress))
+        return;
+
+    (*m_progress) = currentProgress;
+    emit m_deleter->progress(*m_progress);
 }
--- asyncdeleter.h
+++ asyncdeleter.h
@@ -12,13 +12,31 @@ public:
     bool deleteFiles(QStringList flist);
 
 signals:
-    void progress(int p);
+    void progress(int p) const;
 
 protected:
     virtual void run();
 
 private:
     QStringList m_localFiles;
+
+    struct FolderTreeElement
+    {
+        FolderTreeElement(const AsyncDeleter *deleter, qint64 *totalElements, qint64 *remainsElements, int *progress);
+
+        QStringList m_files;
+        std::map<QString, FolderTreeElement> m_folders;
+
+        const AsyncDeleter *m_deleter = nullptr;
+        qint64 *m_totalElements = nullptr;
+        qint64 *m_remainsElements = nullptr;
+        int *m_progress = nullptr;
+
+        void addFilePath(const QString &path);
+        void addFilePathAsList(const QStringList &foldersList, const QString &fname);
+        void deleteFolderTree(const QString &rootFolder = "");
+        void decreaseProgress();
+    };
 };
 
 #endif // ASYNCDELETER_H
